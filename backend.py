85cdd97185cc35b4b972470c53896d5a79c115ff
Alex S <ac.official.it@gmail.com>
2024-11-25 20:43:12 +0300

    -ADD api get data clear, env storage

--- backend.py
+++ backend.py
@@ -5,6 +5,7 @@ import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import dotenv_values
 
 
 class FormData(BaseModel):
@@ -22,7 +23,7 @@ class SleepDataBackend:
         self.setup_logging()
         self.app = FastAPI()
         self.setup_routes()
-        self.PATH = 'data/Sleep_health_and_lifestyle_dataset.csv'
+        self.PATH = dotenv_values('.env')['PATH']
 
 
     def setup_logging(self):
@@ -42,20 +43,52 @@ class SleepDataBackend:
         @self.app.post("/api/submit/")
         async def submit_data(data: FormData):
             return await self.submit_data(data)
-        
+
+
+        @self.app.get('/api/clean_data')
+        async def clean_data():
+            return await self.clean_data()
+
+
         @self.app.get('/api/')
         async def main():
             return {'message': "Hello from FastAPI."}
-       
+
+
+    async def clean_data(self):
+        try:
+            df = pd.read_csv(self.PATH)
+            self.logger.info('Origin data loaded')
+
+            columns_to_delete = dotenv_values('.env')['COLUMNS_TO_DELETE']
+            df.drop(columns=columns_to_delete, inplace=True, errors='ignore')
+            self.logger.info('Useless columns removed')
+
+            row_keep = []
+            for index, row in df.iterrows():
+                if not (row.isna().all() or (row == 0).all()):
+                    row_keep.append(index)
+
+            df = df.loc[row_keep]
+            self.logger.info('Empty lines deleted.')
+
+            df.to_csv(self.PATH, index=False)
+            self.logger.info('Changes are saved')
+
+            return {'message': 'Data successfully cleared.', 'data': df.head().to_dict()}
+        except Exception as e:
+            self.logger.error(f"Error while clearing data: {e}")
+            return {'error': str(e)}
+
 
     async def submit_data(self, data: FormData):
         self.logger.info(f"Received data: {data}")
-        
+
         if not os.path.exists(self.PATH):
             raise FileNotFoundError(f"File not found")
 
         df = pd.read_csv(self.PATH)
-        
+
         last_person_id = df["Person ID"].max()
         new_person_id = last_person_id + 1 if not pd.isnull(last_person_id) else 1
 
